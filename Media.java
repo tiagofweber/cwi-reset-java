20779c12a3979a7a71d5356f3d8b085e5a365419
Tiago Weber <webertiagof@gmail.com>
2021-08-15 14:42:24 -0300

    Create class Media

---
+++ Media.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Media {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite a primeira nota:");
+		double nota1 = scan.nextDouble();
+
+		System.out.println("Digite a segunda nota:");
+		double nota2 = scan.nextDouble();
+
+		System.out.println("Digite a terceira nota:");
+		double nota3 = scan.nextDouble();
+
+		System.out.println("Digite a quarta nota:");
+		double nota4 = scan.nextDouble();
+
+		double soma = nota1 + nota2 + nota3 + nota4;
+		double media = soma / 4;
+
+		System.out.println("A média das notas é:" + media);
+	}
+}
