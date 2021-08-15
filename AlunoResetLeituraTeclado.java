8410f6da8e4991f42f8906b5363949782920353d
Tiago Weber <webertiagof@gmail.com>
2021-08-15 13:25:45 -0300

    Create class AlunoResetLeituraTeclado

---
+++ AlunoResetLeituraTeclado.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AlunoResetLeituraTeclado {
+
+	public static void main (String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite seu nome:");
+		String nome = scan.nextLine();
+
+		System.out.println("Sou aluno do Level 1 do CWI Reset! Me chamo " + nome);
+	}
+}
